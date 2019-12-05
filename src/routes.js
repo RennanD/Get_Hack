115dd3e99a54d8e2e1231bf19005ec2b6d2502e6
RennanD <rplaykl@gmail.com>
2019-12-04 23:13:43 -0300

    Create feature to register users in aplication

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
 
+import User from "./app/controllers/UserController";
+import UserController from "./app/controllers/UserController";
+
 const routes = new Router();
+
+routes.post("/users", UserController.store);
 
 export default routes;
